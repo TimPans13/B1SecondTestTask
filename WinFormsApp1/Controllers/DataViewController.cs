753bb3b773ff4f55ace1c140396f573974eb3ce9
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-12-14 23:21:08 +0300

    add view controller

---
+++ WinFormsApp1/Controllers/DataViewController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WinFormsApp1.Data;
+using WinFormsApp1.Models;
+
+namespace WinFormsApp1.Controllers
+{
+    internal class DataViewController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DataViewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<FileModel> GetFileContentByNameAsync(string fileName)
+        {
+            var result = await _context.Files
+                .Include(f => f.Header)
+                .Include(f => f.ClassList)
+                    .ThenInclude(c => c.ClassAccounts)
+                .Include(f => f.Balance)
+                .FirstOrDefaultAsync(f => f.FileName == fileName);
+
+            return result;
+        }
+
+
+        public List<string> GetFileNamesFromDatabase()
+        {
+            var fileNames = _context.Files.Select(file => file.FileName).ToList();
+            return fileNames;
+        }
+        
+
+    }
+}
