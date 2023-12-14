ef8c7111c0da7ecb1c286e552de52c0279a1d3c5
Tim_Pans_13 <95716918+chort13@users.noreply.github.com>
2023-12-14 23:21:56 +0300

    add FileNames Reader

---
+++ WinFormsApp1/Services/FormServices/DatabaseInteraction.cs
@@ -0,0 +1,24 @@
+﻿using WinFormsApp1.Controllers;
+using WinFormsApp1.Data;
+
+namespace WinFormsApp1
+{
+    public partial class Form1
+    {
+        private List<string> GetFileNamesFromDatabase()
+        {
+            try
+            {
+                ApplicationDbContext dbContext = new ApplicationDbContext();
+                var dataViewController = new DataViewController(dbContext);
+
+                return dataViewController.GetFileNamesFromDatabase();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при получении имен файлов из базы данных: {ex.Message}");
+                return new List<string>();
+            }
+        }
+    }
+}
